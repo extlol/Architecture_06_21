9617dd4210091afe02b9f8d3229b673b5d1cf4a9
extlol <extlol@me.com>
2021-07-01 21:01:29 +0300

    add datasource code config

---
+++ src/main/java/arch/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package arch.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+    @Value("${spring.datasource.username}")
+    private String login;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource getDataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("oracle.jdbc.driver.OracleDriver")
+                .username(login)
+                .password(password)
+                .url("jdbc:oracle:thin:@localhost:1521:test")
+                .build();
+    }
+
+}
